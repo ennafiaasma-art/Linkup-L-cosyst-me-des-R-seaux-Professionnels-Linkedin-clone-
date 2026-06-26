63a371989d892ad15998f309a6427865229441f3
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-26 09:58:03 +0100

    modification  de model  post

--- linkup/app/Models/Post.php
+++ linkup/app/Models/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['content', 'user_id']; // Bach n-goulou l Laravel hado y9dro yt-amodifiaw
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
     //
 }
