69d6dcd2d041f143e3da4c90a8d4c6ddca6d55e6
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-30 15:53:38 +0100

    la creation de fichier login et register dans views

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 Route::get('/feed', [PostController::class, 'index'])->name('feed');
-Route::get('/register',[])->name('show.register');
-Route::get('/login',[])->name("show.login");
+Route::get('/register',[AuthController::class, 'showRegister'])->name('show.register');
+Route::get('/login',[AuthController::class, 'showLogin'])->name("show.login");
