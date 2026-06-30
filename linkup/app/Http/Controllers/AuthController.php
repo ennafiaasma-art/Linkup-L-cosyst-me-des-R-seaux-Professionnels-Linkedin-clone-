6dda5ca8056ad77fd9c6553ebe2f358fcad1f759
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-30 17:36:59 +0100

    la validation des champs au fonction register

--- linkup/app/Http/Controllers/AuthController.php
+++ linkup/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -12,6 +13,20 @@ class AuthController extends Controller
     }
     public function showLogin(){
         return  view('auth.login');
+
+    }
+    public function Register(Request $request){
+    $validated = $request->validate([
+    'name' => 'required|string |max:255',
+    'emai'=> 'required|email|unique:users',
+    'password ' => 'required |string|min:8|confirmed'
+
+    ]);
+    User::create($validated);
+
+    }
+     public function Login(){
+
 
     }
 
