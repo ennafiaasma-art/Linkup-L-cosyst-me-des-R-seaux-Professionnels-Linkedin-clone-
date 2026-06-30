bd0044ae2fa60e8f4f9fc6a425bdc7f39ae5bfa7
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-30 15:40:38 +0100

    la creation des routes de authentification

--- linkup/app/Http/Controllers/PostController.php
+++ linkup/app/Http/Controllers/PostController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\Post;
+
 
 class PostController extends Controller
 {
     //
     public function index()
     {
-        // 1. Nzido 'with('user')' bach ndiro Eager Loading (katمنع n+1 problem f SQL)
-        // 2. latest() kat-triyi l-posts mn l-jdid l-9dim (created_at DESC)
+
         $posts = Post::with('user')->latest()->get();
-        
+
 
         // 3. n-sifto l-posts l l-vue li smيتها feed
         return view('feed', compact('posts'));
--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 Route::get('/feed', [PostController::class, 'index'])->name('feed');
+Route::get('/register',[])->name('show.register');
+Route::get('/login',[])->name("show.login");
