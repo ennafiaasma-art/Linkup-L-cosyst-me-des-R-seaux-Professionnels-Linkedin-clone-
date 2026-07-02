ff38bb170f366fc6a6afefcd061512819784aaec
ennafiaasma-art <ennafiaasma@gmail.com>
2026-07-02 12:19:00 +0100

    la creation des route ajouter et supprimer

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -1,14 +1,27 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+
 Route::get('/feed', [PostController::class, 'index'])->name('feed');
 
-Route::get('/register',[AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login',[AuthController::class, 'showLogin'])->name("show.login");
+Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+Route::post('/register', [AuthController::class, 'Register'])->name('register');
 
-Route::post('/register',[AuthController::class, 'Register'])->name('register');
-Route::post('/login',[AuthController::class, 'Login'])->name("login");
+Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+Route::post('/login', [AuthController::class, 'Login'])->name('login');
 
-Route::post('/logout',[AuthController::class, 'logout'])->name("logout");
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+});
